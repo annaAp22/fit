0f8ef36f577db8062b4408a47f675fd018a74c21
Valdemar404 <bukvinovmmmedia@gmail.com>
2017-06-23 12:16:33 +0300

    fix errors pages

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,15 +15,22 @@ class ComposerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
-        // Using Closure based composers...
-        View::composer('*', function ($view) {
-            $defer = \Session::get('products.defer') ?: [];
-            $seen = \Session::get('products.view') ?: [];
-            $view->with('defer', array_keys($defer) )
-                    ->with('seen', array_keys($seen) );
-        });
-
+      // Using Closure based composers...
+      View::composer('*', function ($view) {
+        $defer = \Session::get('products.defer') ?: [];
+        $seen = \Session::get('products.view') ?: [];
+        $view->with('defer', array_keys($defer) )
+            ->with('seen', array_keys($seen) );
+      });
+      View::composer(['errors::403','errors.403','errors::404','errors.404', 'errors::500', 'errors.500', 'errors::503', 'errors.503'], function ($view) {
+        $settings = Setting::all();
+        $result = collect();
+        foreach($settings as $setting) {
+          $setting->valueUnmarshall();
+          $result->put($setting->var, $setting);
+        }
+        $view->with('global_settings', $result);
+      });
 
     }
 
