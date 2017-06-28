a30415c660217a16bac8a4cd74522fe191c1586e
Valdemar404 <bukvinovmmmedia@gmail.com>
2017-06-28 16:51:32 +0300

    fix favicons

---
+++ resources/views/blocks/favicons.blade.php
@@ -0,0 +1,4 @@
+<link rel="shortcut icon" href="/img/favicon/favicon.ico?v=1" type="image/x-icon"/>
+<link rel="apple-touch-icon" href="/img/favicon/apple-touch-icon.png?v=1"/>
+<link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-touch-icon-72x72.png?v=1"/>
+<link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/apple-touch-icon-114x114.png?v=1"/>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,11 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="yandex-verification" content="c0c2b889beb7fe31" />
-    <link rel="shortcut icon" href="/img/favicon/favicon.ico?v=1" type="image/x-icon"/>
-    <link rel="apple-touch-icon" href="/img/favicon/apple-touch-icon.png"/>
-    <link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-touch-icon-72x72.png"/>
-    <link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/apple-touch-icon-114x114.png"/>
-
+    @include('blocks.favicons')
 	<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
 </head>
 <body>
