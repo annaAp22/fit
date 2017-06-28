be126b73190253f54adca1833a90cd033e48343c
Valdemar404 <bukvinovmmmedia@gmail.com>
2017-06-28 11:42:15 +0300

    move vk pixel in counters

--- resources/views/blocks/head-counters.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- BEGIN head counters -->
-@if(env('IS_LOCALHOST', '0') === '0')
-<!-- BEGIN vk pixel -->
-<script type="text/javascript">(window.Image ? (new Image()) : document.createElement('img')).src = 'https://vk.com/rtrg?p=VK-RTRG-23564-eYnAF';</script>
-<!-- END vk pixel -->
-@endif
-<!-- END head counters -->
