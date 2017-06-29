b33079a4798da31b2f409ca341f5dc1f34f38263
Valdemar404 <bukvinovmmmedia@gmail.com>
2017-06-29 16:50:28 +0300

    add fix initial data, add 2 rows

--- database/seeds/AttributeSeeder.php
+++ database/seeds/AttributeSeeder.php
@@ -6,41 +6,64 @@ use App\Models\Attribute;
 
 class AttributeSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Attribute::firstOrCreate([
-            'type'      => 'color',
-            'name'      => 'Основной цвет',
-            'unit'      => '',
-            'list'      => '',
-            'is_filter' => true,
-            'status'    => 1
-        ]);
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Attribute::firstOrCreate([
+        'type'      => 'color',
+        'name'      => 'Основной цвет',
+        'unit'      => '',
+        'list'      => '',
+        'is_filter' => true,
+        'status'    => 1
+    ]);
 
-        Attribute::firstOrCreate([
-            'type'      => 'color',
-            'name'      => 'Дополнительный цвет',
-            'unit'      => '',
-            'list'      => '',
-            'is_filter' => true,
-            'status'    => 1
-        ]);
+    Attribute::firstOrCreate([
+        'type'      => 'color',
+        'name'      => 'Дополнительный цвет',
+        'unit'      => '',
+        'list'      => '',
+        'is_filter' => true,
+        'status'    => 1
+    ]);
 
-        $sizes = [];
-        for($i = 38; $i <= 50; $i += 2) $sizes[] = $i;
+    $sizes = [];
+    for($i = 38; $i <= 50; $i += 2) $sizes[] = $i;
+    if(!Attribute::where('name', 'Размеры')->first())
+      Attribute::firstOrCreate([
+          'type'      => 'checklist',
+          'name'      => 'Размеры',
+          'unit'      => '',
+          'list'      => json_encode($sizes),
+          'is_filter' => true,
+          'status'    => 1,
+      ]);
+    $sizes = [];
+    for($i = 38; $i <= 48; $i += 2) $sizes[] = $i;
+    if(!Attribute::where('name', 'Женские размеры')->first())
+      Attribute::firstOrCreate([
+          'type'      => 'checklist',
+          'name'      => 'Женские размеры',
+          'unit'      => '',
+          'list'      => json_encode($sizes),
+          'is_filter' => false,
+          'status'    => 1,
+      ]);
+    $sizes = [];
+    for($i = 44; $i <= 54; $i += 2) $sizes[] = $i;
+    if(!Attribute::where('name', 'Мужские размеры')->first())
+      Attribute::firstOrCreate([
+          'type'      => 'checklist',
+          'name'      => 'Мужские размеры',
+          'unit'      => '',
+          'list'      => json_encode($sizes),
+          'is_filter' => false,
+          'status'    => 1,
+      ]);
 
-        Attribute::firstOrCreate([
-            'type'      => 'checklist',
-            'name'      => 'Размеры',
-            'unit'      => '',
-            'list'      => json_encode($sizes),
-            'is_filter' => false,
-            'status'    => 1,
-        ]);
-    }
+  }
 }
