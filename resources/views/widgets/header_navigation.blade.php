d2e5d48f6657ae24ccdf8429284d8c062661e712
Valdemar404 <bukvinovmmmedia@gmail.com>
2017-08-15 14:14:01 +0300

    rename third caption

--- resources/views/widgets/header_navigation.blade.php
+++ resources/views/widgets/header_navigation.blade.php
@@ -8,8 +8,8 @@
         <div class="nav-catalog__item js-toggle-active-target js-men js-catalog" data-target=".js-men" data-reset=".js-women, .js-training"><span>ДЛЯ МУЖЧИН</span><i class="sprite_main sprite_main-icon__arrow_green_down"></i>
         </div>
         @endif
-        @if($categories->contains('name', 'Для тренировок'))
-        <div class="nav-catalog__item nav-catalog__item_wide js-toggle-active-target js-training js-catalog" data-target=".js-training" data-reset=".js-women, .js-men"><span>ДЛЯ ТРЕНИРОВОК</span><i class="sprite_main sprite_main-icon__arrow_green_down"></i>
+        @if($categories->contains('name', 'Тип тренировок'))
+        <div class="nav-catalog__item nav-catalog__item_wide js-toggle-active-target js-training js-catalog" data-target=".js-training" data-reset=".js-women, .js-men"><span>Тип тренировок</span><i class="sprite_main sprite_main-icon__arrow_green_down"></i>
         </div>
         @endif
     </div>
