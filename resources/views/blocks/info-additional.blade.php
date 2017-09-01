dedb2a74e8ab40fd40b48193225aaed372cc8692
Alexander <kamyshny.mmmedia@gmail.com>
2017-09-01 10:04:33 +0300

    Look Book in info menu

--- resources/views/blocks/info-additional.blade.php
+++ resources/views/blocks/info-additional.blade.php
@@ -1,3 +1,4 @@
 <li><a href="{{ route('photos') }}">Фото клиентов</a></li>
 {{--<li><a href="{{ route('delivery') }}">Доставка и оплата</a></li>--}}
 <li><a href="{{ route('articles') }}">Статьи</a></li>
+<li><a href="{{ route('look_book') }}">Look Book</a></li>
