b0441a1bf1fd4e7399a635f19fd2ee1118279bcd
Alexander <kamyshny.mmmedia@gmail.com>
2017-09-05 09:58:18 +0300

    Contacts page scheme link opens image

--- resources/views/blocks/contacts-scheme.blade.php
+++ resources/views/blocks/contacts-scheme.blade.php
@@ -1,10 +1,12 @@
 @if(isset($page->photos) && $page->photos->count())
-    <div class="contacts__gallery" id="#scheme">
+    <div class="contacts__gallery">
         <div class="container-in">
+            @php $scheme = 'id=scheme'; @endphp
             @foreach($page->photos->where('name', 'scheme') as $photo)
-                <a href="{{ $photo->uploads->img->url() }}" data-fancybox="group-0">
+                <a href="{{ $photo->uploads->img->url() }}" data-fancybox="group-0" {{ $scheme }}>
                     <img class="page-text__image" src="{{ $photo->uploads->img->preview->url() }}" alt="">
                 </a>
+                @php $scheme = ''; @endphp
             @endforeach
         </div>
     </div>
