a28c0e46a6ff27ca63b979ffaa332e9ab2c53874
Alexander Nekipelov <support@reflexit.ru>
2017-10-16 05:04:04 -0400

    Добавлен счётчик roistat

--- resources/views/blocks/counters.blade.php
+++ resources/views/blocks/counters.blade.php
@@ -107,3 +107,14 @@
     </script>
 @endif
 <!-- END counters-->
+
+<!--Roistat Counter-->
+<script>
+(function(w, d, s, h, id) {
+    w.roistatProjectId = id; w.roistatHost = h;
+    var p = d.location.protocol == "https:" ? "https://" : "http://";
+    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init";
+    var js = d.createElement(s); js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+})(window, document, 'script', 'cloud.roistat.com', '3959f36da3eb04abd98a563f6bd55ef4');
+</script>
+<!--End Roistat counter-->
