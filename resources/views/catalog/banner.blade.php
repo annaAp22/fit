54222bbe178be608b99a0385e9c74aab57dbf0d8
Valdemar404 <bukvinovmmmedia@gmail.com>
2017-11-27 10:08:36 +0300

    hide banner

--- resources/views/catalog/banner.blade.php
+++ resources/views/catalog/banner.blade.php
@@ -1,6 +1,5 @@
-@if(Route::currentRouteName() == 'actions')
-<div style="width: 100%;">
-    {{--<img style="margin: 0 0 30px; width:100%; height: auto;" src="/img/black-friday-1155-min.jpg" alt="">--}}
-    <iframe src="/plugins/css3-countdown-master/changed.html" height="286" width="100%" scrolling="no" style="border:none;"></iframe>
-</div>
-@endif
+{{--@if(Route::currentRouteName() == 'actions')--}}
+{{--<div style="width: 100%;">--}}
+    {{--<iframe src="/plugins/css3-countdown-master/changed.html" height="286" width="100%" scrolling="no" style="border:none;"></iframe>--}}
+{{--</div>--}}
+{{--@endif--}}
